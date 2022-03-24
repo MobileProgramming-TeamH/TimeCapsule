d9d65bfc29d93e39f805075e631990ba0f5e3c15
danpung2 <danpung_2@naver.com>
2022-03-24 22:33:16 +0900

    Calender View

--- app/src/main/java/com/gachon/timecapsule/MainActivity.java
+++ app/src/main/java/com/gachon/timecapsule/MainActivity.java
@@ -3,6 +3,8 @@ package com.gachon.timecapsule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendar = (CalendarView)findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, year + "년 " + month + "월" + dayOfMonth + "일", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
